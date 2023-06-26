Fix an survival calculation error in rmtpp.py.
Also, I tried to fix the wrong intensity value due to distribution
transformation.

src/TPP/model/rmtpp/model.py
@@ -150,7 +150,7 @@ class RMTPP(BasicModule):
         dummy_event_index = mask_next.sum(dim = -1) - 1                        # [batch_size]
         probability_survival = integral_time_next.gather(index = dummy_event_index.unsqueeze(dim = -1), dim = -1)
                                                                                # [batch_size, 1]
-        time_loss_survival = -torch.log(probability_survival + self.epsilon).mean()
+        time_loss_survival = probability_survival.mean()
 
         return time_loss_time_next_without_dummy, time_loss_survival, events_loss_time_next_without_dummy, \
                mae, f1, the_number_of_events

src/TPP/model/rmtpp/rmtpp.py
@@ -78,7 +78,7 @@ class RMTPPModule(nn.Module):
 
         # Get the intensity function and corresponding integral.
         intensity = torch.exp(time_scalar * time_next) * constant              # [batch_size, seq_len, 1]
-        integral = (intensity - constant) / time_scalar                        # [batch_size, seq_len, 1]
+        integral = (intensity - constant) / time_scalar * var                  # [batch_size, seq_len, 1]
 
         mark = None
         if self.event_toggle:
@@ -125,7 +125,7 @@ class RMTPPModule(nn.Module):
         expanded_time = original_time_expand_normed.unsqueeze(dim = -2)        # [batch_size, seq_len, 1, resolution]
         
         intensity_events = torch.exp(time_scalar * expanded_time) * constant   # [batch_size, seq_len, 1, resolution]
-        integral_events = (intensity_events - constant) / time_scalar          # [batch_size, seq_len, 1, resolution]
+        integral_events = (intensity_events - constant) / time_scalar * var    # [batch_size, seq_len, 1, resolution]
 
         # aggregated timestamp
         batch_size, seq_len, _ = original_time_expand.shape
@@ -174,7 +174,7 @@ class RMTPPModule(nn.Module):
         expanded_time = original_time_expand_normed.unsqueeze(dim = -2)        # [batch_size, seq_len, 1, resolution]
         
         intensity_events = torch.exp(time_scalar * expanded_time) * constant   # [batch_size, seq_len, 1, resolution]
-        integral_events = (intensity_events - constant) / time_scalar          # [batch_size, seq_len, 1, resolution]
+        integral_events = (intensity_events - constant) / time_scalar * var    # [batch_size, seq_len, 1, resolution]
 
         # aggregated timestamp
         batch_size, seq_len, _ = original_time_expand.shape