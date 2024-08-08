Fix wrong timestamp calculation in CTLSTM and FENN.

src/TPP/model/ctlstm/submodel.py
@@ -126,13 +126,7 @@ class CTLSTM(nn.Module):
         expanded_integral_all_events = approximate_integration(expanded_intensity_all_events, expanded_time, dim = -2)
                                                                                # [batch_size, seq_len, integration_sample_rate, num_events]
 
-        # Obtain timestamp
-        timestamp, timestamp_ps = pack(
-            (torch.zeros_like(time_next), expanded_time.diff(dim = -1)),
-            'b s *'
-        )                                                                      # [batch_size, seq_len, integration_sample_rate]
-
-        return expanded_integral_all_events, expanded_intensity_all_events, timestamp
+        return expanded_integral_all_events, expanded_intensity_all_events, expanded_time
 
 
     def integral_intensity_time_next_3d(self, events_history, time_history, time_next, integration_sample_rate, num_dimension_prior_batch = 0):
@@ -156,12 +150,7 @@ class CTLSTM(nn.Module):
         expanded_integral_all_events = approximate_integration(expanded_intensity_all_events, expanded_time, dim = -2)
                                                                                # [..., batch_size, seq_len, num_events, integration_sample_rate, num_events]
 
-        # Obtain timestamp
-        timestamp = torch.concat(
-            [torch.zeros_like(time_next).unsqueeze(dim = -1), expanded_time.diff(dim = -1)], dim = -1
-        )                                                                      # [..., batch_size, seq_len, num_events, integration_sample_rate]
-
-        return expanded_integral_all_events, expanded_intensity_all_events, timestamp
+        return expanded_integral_all_events, expanded_intensity_all_events, expanded_time
 
 
     def model_probe_function(self, events_history, time_history, time_next, mask_next, integration_sample_rate):
@@ -184,12 +173,6 @@ class CTLSTM(nn.Module):
                                                                                # [batch_size, seq_len, integration_sample_rate, num_events]
         expanded_integral_all_events = approximate_integration(expanded_intensity_all_events, expanded_time, dim = -2)
                                                                                # [batch_size, seq_len, integration_sample_rate, num_events]
-
-        # Obtain timestamp
-        timestamp, timestamp_ps = pack(
-            (torch.zeros_like(time_next), expanded_time.diff(dim = -1)),
-            'b s *'
-        )                                                                      # [batch_size, seq_len, integration_sample_rate]
         
         # construct the plot dict
         data = {}
@@ -238,4 +221,4 @@ class CTLSTM(nn.Module):
         data['pearson_matrix'] = pearson_matrix
         data['L1_matrix'] = L1_matrix
         
-        return data, timestamp
+        return data, expanded_time